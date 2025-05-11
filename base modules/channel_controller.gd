caa2cd4c111301f6ebef582ef1443a1a6a35564a
Tabby <41929769+tabby-cat-nya@users.noreply.github.com>
2025-05-11 17:08:32 +1000

    camera channels zoom out effect looks very feasible

---
+++ base modules/channel_controller.gd
@@ -0,0 +1,24 @@
+extends Control
+class_name Channel
+
+
+@export var channel_name : String
+@export_group("Node References")
+@export var offline_channel_cover : TextureRect
+@export var game_viewport : SubViewport
+
+var channel_online : bool
+
+
+
+func start_channel():
+	pass
+
+func end_channel():
+	pass
+
+func win_channel():
+	pass
+
+func lose_channel():
+	pass
